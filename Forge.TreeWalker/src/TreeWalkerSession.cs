5b686f8ce3996ac3474f115d4b6af14c5b5db757
Satish Upadhyaya <saupadhyaya@microsoft.com>
2025-08-26 17:29:05 -0600

    remove linq reference

--- Forge.TreeWalker/src/TreeWalkerSession.cs
+++ Forge.TreeWalker/src/TreeWalkerSession.cs
@@ -13,7 +13,6 @@ namespace Microsoft.Forge.TreeWalker
     using System.Collections;
     using System.Collections.Generic;
     using System.Diagnostics;
-    using System.Linq;
     using System.Reflection;
     using System.Text.RegularExpressions;
     using System.Threading;
